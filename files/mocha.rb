bd6f0fafe3e39b9d8f4bcdfc77e872809534585a
Adam Phillips <aphillips@scholastic.co.uk>
2016-09-29 22:12:08 +0100

    Make asset setup optional

---
+++ files/mocha.rb
@@ -0,0 +1,1 @@
+require 'mocha/mini_test'
--- template.rb
+++ template.rb
@@ -60,13 +60,13 @@ append_to_file '.gitignore', "\n.env\n"
 template 'files/capybara.rb', 'test/support/capybara.rb'
 template 'files/factory_girl.rb', 'test/support/factory_girl.rb'
 template 'files/minitest_reporters.rb', 'test/support/minitest_reporters.rb'
+template 'files/mocha.rb', 'test/support/mocha.rb'
 
 insert_into_file 'test/test_helper.rb', after: "require 'rails/test_help'\n\n" do
   [
     "require 'minitest/autorun'",
-    "",
-    "require 'support/factory_girl'",
-    "require 'support/capybara'",
+    "require 'support/minitest_reporters'",
+    "require 'support/mocha'",
     "",
     ""
   ].join("\n")
@@ -77,48 +77,52 @@ git commit: %Q{-m 'Configured test environment'}
 
 # Setup bower
 
-run('npm init')
+setup_assets = yes?('Setup assets using bower?')
 
-run('npm install --save-dev bower')
+if setup_assets
+  run('npm init')
 
-insert_into_file 'package.json', before: '  "devDependencies"' do
-  [
-    '  "scripts": {',
-    '    "bower": "node_modules/bower/bin/bower"',
-    '  },'
-  ].join("\n")
+  run('npm install --save-dev bower')
+
+  insert_into_file 'package.json', before: '  "devDependencies"' do
+    [
+      '  "scripts": {',
+      '    "bower": "node_modules/bower/bin/bower"',
+      '  },'
+    ].join("\n")
+  end
+
+  run('npm run bower init')
+
+  append_to_file '.gitignore', "\nnode_modules\n"
+
+  template 'files/bowerrc', '.bowerrc'
+  empty_directory 'vendor/assets/components'
+
+  append_to_file 'config/initializers/assets.rb' do
+    [
+      '',
+      '# Add bower components to the asset load path',
+      "Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'components')"
+    ].join("\n")
+  end
+
+  git add: '.'
+  git commit: %Q{-m 'Configured bower for front-end dependencies'}
+
+  # Setup bootstrap
+
+  run('npm run bower install bootstrap-sass')
+
+  remove_file 'app/assets/javascripts/application.js'
+  remove_file 'app/assets/stylesheets/application.css'
+
+  template 'files/application.js', 'app/assets/javascripts/application.js'
+  template 'files/application.scss', 'app/assets/stylesheets/application.scss'
+  template 'files/bootstrap.rb', 'config/initializers/bootstrap.rb'
+
+  git add: '.'
+  git commit: %Q{-m 'Setup bootstrap'}
+
+  # Reconfigure default assets
 end
-
-run('npm run bower init')
-
-append_to_file '.gitignore', "\nnode_modules\n"
-
-template 'files/bowerrc', '.bowerrc'
-empty_directory 'vendor/assets/components'
-
-append_to_file 'config/initializers/assets.rb' do
-  [
-    '',
-    '# Add bower components to the asset load path',
-    "Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'components')"
-  ].join("\n")
-end
-
-git add: '.'
-git commit: %Q{-m 'Configured bower for front-end dependencies'}
-
-# Setup bootstrap
-
-run('npm run bower install bootstrap-sass')
-
-remove_file 'app/assets/javascripts/application.js'
-remove_file 'app/assets/stylesheets/application.css'
-
-template 'files/application.js', 'app/assets/javascripts/application.js'
-template 'files/application.scss', 'app/assets/stylesheets/application.scss'
-template 'files/bootstrap.rb', 'config/initializers/bootstrap.rb'
-
-git add: '.'
-git commit: %Q{-m 'Setup bootstrap'}
-
-# Reconfigure default assets
